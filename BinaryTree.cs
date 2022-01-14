31e44b4fd5eb0077df7d6ddb0cc5b4e63ab7815d
Larrykatuva <larry.katuva@gmail.com>
2022-01-14 12:33:53 +0300

    All solution

---
+++ BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree<T>
+    {
+        private T value;
+        public BinaryTree<T> LeftChild { get; private set; }
+
+        public BinaryTree<T> RightChild { get; private set; }
+
+        public BinaryTree(T value, BinaryTree<T> leftChild, BinaryTree<T> rightChild)
+        {
+            this.value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public BinaryTree (T value): this(value, null, null)
+        {
+
+        }
+
+        public void PrintInOrder()
+        {
+            if(this.LeftChild != null)
+            {
+                this.LeftChild.PrintInOrder();
+            }
+            Console.Write(this.value + " ");
+            if(this.RightChild != null)
+            {
+                this.RightChild.PrintInOrder();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> binaryTree =
+                new BinaryTree<int>(14,
+                    new BinaryTree<int>(19,
+                        new BinaryTree<int>(23),
+                        new BinaryTree<int>(6,
+                            new BinaryTree<int>(10),
+                     new BinaryTree<int>(21))),
+                     new BinaryTree<int>(15,
+                new BinaryTree<int>(3),
+             null));
+
+            binaryTree.PrintInOrder();
+            Console.WriteLine();
+
+        }
+    }
+}
